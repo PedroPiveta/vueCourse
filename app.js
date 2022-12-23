ac582295e324f5a19050f00147746c9b7f56ce6b
PedroPiveta <ppivetabarrotti208@gmail.com>
2022-12-23 08:48:50 -0300

    mouse events

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45 
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
@@ -17,7 +19,17 @@ const app = Vue.createApp({
         
         toggleShowBooks() {
             this.showBooks = !this.showBooks
-        }
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
     }
 })
 
