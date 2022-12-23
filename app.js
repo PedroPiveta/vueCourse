47dc2687c734f06c30d492c28de2b6f4532ad8fd
PedroPiveta <ppivetabarrotti208@gmail.com>
2022-12-23 09:14:50 -0300

    dynamic classes

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books : [
-                { title: 'name of the wind', author: 'patric rothfuss', img: 'assets/1.jpg' },
-                { title: 'the way of the kings', author: 'bradon sanderson', img: 'assets/2.jpg'},
-                { title: 'the final empire', author: 'bradon sanderson', img: 'assets/4.jpg'}
+                { title: 'name of the wind', author: 'patric rothfuss', img: 'assets/1.jpg', isFav: true },
+                { title: 'the way of the kings', author: 'bradon sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'bradon sanderson', img: 'assets/3.jpg', isFav: true }
             ],
             x: 0,
             y: 0
